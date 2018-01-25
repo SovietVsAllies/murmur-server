99c06bdab60b9704a4be55181e5a8dfe342805f5
Moandor <moandor.y@gmail.com>
2018-01-25 19:58:46 +0800

    Refactor

--- sm_server/api/views.py
+++ sm_server/api/views.py
@@ -2,8 +2,8 @@ import base64
 import binascii
 import uuid
 
-from django.db import transaction, IntegrityError
-from rest_framework import status, viewsets
+from django.db import transaction
+from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
@@ -20,7 +20,7 @@ class AccountViewSet(viewsets.ViewSet):
             account.save()
             return Response(AccountSerializer(account).data)
         except KeyError as e:
-            raise ValidationError('%s is required' % e)
+            raise ValidationError({'code': -100, 'message': '%s is required' % e})
 
 
 class PreKeyViewSet(viewsets.ViewSet):
@@ -37,9 +37,10 @@ class PreKeyViewSet(viewsets.ViewSet):
                     if pre_key.exists():
                         pre_key.delete()
                     PreKey(account=account, key_id=key_id, key=key).save()
+            return Response({'code': 0})
         except (ValueError, binascii.Error) as e:
-            raise ValidationError('Invalid request: %s' % e)
+            raise ValidationError({'code': -100, 'message': 'Invalid request: %s' % e})
         except Account.DoesNotExist:
-            raise ValidationError('Account does not exist')
+            raise ValidationError({'code': -101, 'message': 'Account does not exist'})
         except KeyError as e:
-            raise ValidationError('%s is required' % e)
+            raise ValidationError({'code': -100, 'message': '%s is required' % e})
