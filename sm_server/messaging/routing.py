934f5f9e913d9089696961bed8dbd56e1df4000f
Moandor <moandor.y@gmail.com>
2018-01-26 21:16:39 +0800

    Fix disconnection handling

--- sm_server/messaging/routing.py
+++ sm_server/messaging/routing.py
@@ -1,8 +1,9 @@
 from channels import route
 
-from messaging.consumers import message_connect, message_consumer
+from messaging.consumers import message_connect, message_consumer, message_disconnect
 
 channel_routing = [
     route('websocket.connect', message_connect, path=r'^/message/$'),
     route('websocket.receive', message_consumer, path=r'^/message/$'),
+    route('websocket.disconnect', message_disconnect, path=r'^/message/$'),
 ]
